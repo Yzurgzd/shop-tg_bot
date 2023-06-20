4600a75f2d556dedc53223a92644f3ddc1c31222
Rinat <kyoukohorichan@gmail.com>
2023-06-20 15:10:04 +0300

    first

--- config.py
+++ config.py
@@ -9,11 +9,15 @@ cursor_w = conn_w.cursor()
 
 
 # токен от бота
-token = '1261335792:AAEFt3X79WkjWh5cAMlkeOsUOzIvt1VOP4w'
+token = ''
 
-admin_id = int(cursor_a.execute("SELECT * FROM admin WHERE name='admin_id'").fetchall()[0][1])
-group_id = str(cursor_a.execute("SELECT * FROM admin WHERE name='group_id'").fetchall()[0][1]) # Id группы для оповещений. Добавьте бота в группу @getmyid_bot -375120528
-manager = str(cursor_a.execute("SELECT * FROM admin WHERE name='manager'").fetchall()[0][1])
+admin_id = int(cursor_a.execute(
+    "SELECT * FROM admin WHERE name='admin_id'").fetchall()[0][1])
+# Id группы для оповещений. Добавьте бота в группу @getmyid_bot -375120528
+group_id = str(cursor_a.execute(
+    "SELECT * FROM admin WHERE name='group_id'").fetchall()[0][1])
+manager = str(cursor_a.execute(
+    "SELECT * FROM admin WHERE name='manager'").fetchall()[0][1])
 
 qiwi_number = str(cursor_w.execute("SELECT * FROM wallets").fetchall()[0][0])
 qiwi_token = str(cursor_w.execute("SELECT * FROM wallets").fetchall()[0][1])
